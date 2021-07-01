19c9677cf668bb9127f6bad3dea842ca1327d8ab
Ali Moradi <ali.moradi.design@gmail.com>
2021-07-01 21:40:30 +0430

    add context

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import CollectionContext from '../../contexts/collections/collection.context';
+import CollectionContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
